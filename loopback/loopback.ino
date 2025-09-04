fd6290f1339ddc3ae0d05e6e05a7aea60d307cb9
Anand Anil <anand.anil2004@gmail.com>
2025-09-05 00:08:12 +0530

    FSM implementation done to prevent blocking in emergencies

--- loopback/loopback.ino
+++ loopback/loopback.ino
@@ -5,50 +5,147 @@ Servo Sprayer;
 const int servoPin = 2;
 const int BuzzerPin = 4;
 
+unsigned long stateStartTime = 0;
+unsigned long buzzerTimer = 0;
+bool buzzerActive = false;
+int buzzerCount = 0;
+int maxBuzzerCount = 0;
+
+enum State {
+  IDLE,
+  MEDIUM_BUZZER,
+  HIGH_BUZZER_CONTINUOUS,
+  HIGH_BUZZER_PULSED,
+  SMELL_ATTACK_SERVO
+};
+
+State currentState = IDLE;
+
 void setup() {
   Serial.begin(115200);
   while (!Serial) {;}
   Sprayer.attach(servoPin, 500, 2400);
   Sprayer.write(0);
   Serial.println("Annoyance Machine Ready");
-  pinMode(BuzzerPin,OUTPUT);
-}
-
-void annoydriver(){
-  Serial.println("Annoying with buzzer");
-  for (int i = 0; i<=3; i++){
-    digitalWrite(BuzzerPin,HIGH);
-    delay(500);
-    digitalWrite(BuzzerPin,LOW);
-    delay(500);
-  }
-  Serial.println("Annoyed with buzzer");
-  delay(200);
-}
-
-void attackdriver(){
-  Serial.println("Commencing ultimate attack");
-  Sprayer.write(180);
-  delay(500);
-  Sprayer.write(0);
-  Serial.println("Ultimate attack successful");
-  delay(500);
-  annoydriver();
+  pinMode(BuzzerPin, OUTPUT);
+  digitalWrite(BuzzerPin, LOW);
 }
 
 void loop() {
+  handleSerialCommands();
+  updateStateMachine();
+  delay(1);
+}
+
+void handleSerialCommands() {
   if (Serial.available()) {
-    char incomingMessage = Serial.read();
-    Serial.print("Received: ");
-    Serial.println(incomingMessage);
+    char incomingMessage = Serial.read();    
     switch(incomingMessage) {
-      case 'H':
-        attackdriver();
-        break;
       case 'M':
-        annoydriver();
+        if (currentState == IDLE) {
+          startMediumAlert();
+        }
         break;
-      default: break;
+      case 'H':
+        if (currentState == IDLE) {
+          startHighAlert();
+        }
+        break;
+      case 'S':
+        startSmellAttack();
+        break;
+      default: 
+        break;
     }
+  }
+}
+
+void startMediumAlert() {
+  Serial.println("Annoying with buzzer");
+  currentState = MEDIUM_BUZZER;
+  stateStartTime = millis();
+  buzzerTimer = millis();
+  buzzerCount = 0;
+  maxBuzzerCount = 3;
+  buzzerActive = false;
+}
+
+void startHighAlert() {
+  Serial.println("Continuous Buzzer Mode Active");
+  currentState = HIGH_BUZZER_CONTINUOUS;
+  stateStartTime = millis();
+  digitalWrite(BuzzerPin, HIGH);
+}
+
+void startSmellAttack() {
+  Serial.println("Commencing Ultimate Attack - OVERRIDE ALL");
+  digitalWrite(BuzzerPin, LOW);
+  currentState = SMELL_ATTACK_SERVO;
+  stateStartTime = millis();
+  Sprayer.write(180);
+}
+
+void updateStateMachine() {
+  unsigned long currentMillis = millis();
+  unsigned long elapsed = currentMillis - stateStartTime;
+  unsigned long buzzerElapsed = currentMillis - buzzerTimer;
+  
+  switch(currentState) {
+    case IDLE:
+      break;
+      
+    case MEDIUM_BUZZER:
+      if (buzzerCount < maxBuzzerCount) {
+        if (!buzzerActive && buzzerElapsed >= 1000) {
+          digitalWrite(BuzzerPin, HIGH);
+          buzzerActive = true;
+          buzzerTimer = currentMillis;
+        } else if (buzzerActive && buzzerElapsed >= 500) {
+          digitalWrite(BuzzerPin, LOW);
+          buzzerActive = false;
+          buzzerCount++;
+          buzzerTimer = currentMillis;
+        }
+      } else {
+        currentState = IDLE;
+        digitalWrite(BuzzerPin, LOW);
+      }
+      break;
+      
+    case HIGH_BUZZER_CONTINUOUS:
+      if (elapsed >= 3000) {
+        digitalWrite(BuzzerPin, LOW);
+        currentState = HIGH_BUZZER_PULSED;
+        stateStartTime = currentMillis;
+        buzzerTimer = currentMillis;
+        buzzerCount = 0;
+        buzzerActive = false;
+      }
+      break;
+      
+    case HIGH_BUZZER_PULSED:
+      if (buzzerCount < maxBuzzerCount) {
+        if (!buzzerActive && buzzerElapsed >= 1000) {
+          digitalWrite(BuzzerPin, HIGH);
+          buzzerActive = true;
+          buzzerTimer = currentMillis;
+        } else if (buzzerActive && buzzerElapsed >= 500) {
+          digitalWrite(BuzzerPin, LOW);
+          buzzerActive = false;
+          buzzerCount++;
+          buzzerTimer = currentMillis;
+        }
+      } else {
+        currentState = IDLE;
+        digitalWrite(BuzzerPin, LOW);
+      }
+      break;
+      
+    case SMELL_ATTACK_SERVO:
+      if (elapsed >= 500) {
+        Sprayer.write(0);
+        currentState = IDLE;
+      }
+      break;
   }
 }
